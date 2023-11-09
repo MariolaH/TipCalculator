1c029003c4082111e5629966f91d47006176af26
Mariola <mariolahullings@gmail.com>
2023-11-08 21:10:54 -0500

    set IBAction

---
+++ TipCalculator/Controllers/CalculatorViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Mariola Hullings on 11/8/23.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet var billTextField: UITextField!
+    @IBOutlet var zeroPctButton: UIButton!
+    @IBOutlet var tenPctButton: UIButton!
+    @IBOutlet var twentyPctButton: UIButton!
+    @IBOutlet var splitNumberLabel: UILabel!
+  
+
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+    
+}
+
