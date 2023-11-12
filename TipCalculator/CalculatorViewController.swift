8cea5e69dc62fbd895cde1c16f43e35f9d19f269
Mariola <mariolahullings@gmail.com>
2023-11-12 12:40:12 -0500

    Complete tip calculator

--- TipCalculator/CalculatorViewController.swift
+++ TipCalculator/CalculatorViewController.swift
@@ -8,22 +8,66 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
-
+    
     @IBOutlet var billTextField: UITextField!
     @IBOutlet var zeroPctButton: UIButton!
     @IBOutlet var tenPctButton: UIButton!
     @IBOutlet var twentyPctButton: UIButton!
     @IBOutlet var splitNumberLabel: UILabel!
-  
-
+    var tipAmount: Double = 0.0
+    var printTwoDecimalPoints = "0.0"
+    var numberOfPeople = "2"
+    
+    
     @IBAction func tipChanged(_ sender: UIButton) {
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        
+        sender.isSelected = true
+        //displays tip option as a %
+        let tip = sender.currentTitle!
+        //.dropLast removes the % and String is used to create a new string from the result of tip.dropLast()
+        //This is necessary because the dropLast() method returns a substring
+        let removePercent = String(tip.dropLast())
+        // Convert the removePercent string into a Double? and then forcefully unwraps it. tipInt is of type Double?
+        let tipInt = Double(removePercent)!
+        //Divide tipInt by 100 to get a decimal
+        let decimalTip = tipInt / 100
+        //        print(decimalTip)
+        tipAmount = decimalTip
+        billTextField.endEditing(true)
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        let split = String(format: "%.0f", sender.value)
+        splitNumberLabel.text = split.description
+        numberOfPeople = split
+        
+        //        print(sender.value)
+        
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+   calculate()
+        self.performSegue(withIdentifier: "goToResults", sender: self)
     }
     
+    func calculate() {
+        let howManyWaysToSplit = splitNumberLabel.text!
+        let tippedAmount = (Double(billTextField.text!) ?? 0.0) * tipAmount
+        let check = tippedAmount + (Double(billTextField.text!) ?? 0.0)
+        let calculateSplit = check / Double(howManyWaysToSplit)!
+        printTwoDecimalPoints = String(format: "%.2f", calculateSplit)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResults" {
+            let destinationVC = segue.destination as! ResultsViewController
+            destinationVC.totalPerPerson = printTwoDecimalPoints
+            destinationVC.splitBetween = numberOfPeople
+            destinationVC.tip = tipAmount * 100
+        }
+    }
 }
 
--- TipCalculator/ResultsViewController.swift
+++ TipCalculator/ResultsViewController.swift
@@ -10,13 +10,24 @@ import UIKit
 class ResultsViewController: UIViewController {
     
     
-
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    var totalPerPerson = "0.0"
+    var splitBetween = "2"
+    var tip = 10.0
+    var tipAsInt: Int = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tipAsInt = Int(tip)
+        totalLabel.text = "$ \(totalPerPerson)"
+        settingsLabel.text = "Split between \(splitBetween) people, with \(tipAsInt)% tip."
     }
     
-
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 
 }
